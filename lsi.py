2105533ea7bfaa2e933cb618c55e207876461b8b
Skywave <thakoch@gmail.com>
2013-01-04 20:57:07 +0100

    Reload corpus from tfid

--- lsi.py
+++ lsi.py
@@ -8,6 +8,7 @@ dictionary = corpora.Dictionary.load_from_text('wordid.txt')
 print  'generate tfidf'
 tfidf = models.TfidfModel(corpus, id2word=dictionary, normalize=True)
 corpora.MmCorpus.serialize('tfidf.mm', tfidf[corpus], progress_cnt=10000)
+corpus = corpora.MmCorpus('tfidf.mm')
 print 'generate lsi'
 lsi = models.LsiModel(corpus, id2word=dictionary, num_topics=100)
 index = similarities.MatrixSimilarity(lsi[corpus])
